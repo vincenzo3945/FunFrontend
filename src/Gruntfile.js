7faf4ac29ebc424d23f323f57a0276eea652c811
vincenzo3945 <43238133+vincenzo3945@users.noreply.github.com>
2020-01-06 14:24:08 +0100

    Grunt coverage

---
+++ src/Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    coveralls: {
+    options: {
+        debug: true,
+        coverageDir: 'directory name',
+        dryRun: true,
+        force: true,
+        recursive: true
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.loadNpmTasks('grunt-karma-coveralls');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+    
+};
