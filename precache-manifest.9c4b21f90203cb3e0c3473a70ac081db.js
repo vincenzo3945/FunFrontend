f6ed977f2b0f345b4e3fc3ebd9080100bba66d60
vincenzo3945 <43238133+vincenzo3945@users.noreply.github.com>
2020-01-13 14:18:59 +0100

    Updates

---
+++ precache-manifest.9c4b21f90203cb3e0c3473a70ac081db.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea34478e67bb6e0b1387eb2f783c03e",
+    "url": "/FunFrontend/index.html"
+  },
+  {
+    "revision": "b9048f3003145dfb830b",
+    "url": "/FunFrontend/static/css/2.dd5f9282.chunk.css"
+  },
+  {
+    "revision": "34cc6ee9e80a815b7450",
+    "url": "/FunFrontend/static/css/main.b63ed3d6.chunk.css"
+  },
+  {
+    "revision": "b9048f3003145dfb830b",
+    "url": "/FunFrontend/static/js/2.1cd93fac.chunk.js"
+  },
+  {
+    "revision": "4191ec6efa422a685df893c6af95f05e",
+    "url": "/FunFrontend/static/js/2.1cd93fac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34cc6ee9e80a815b7450",
+    "url": "/FunFrontend/static/js/main.5e9a9132.chunk.js"
+  },
+  {
+    "revision": "79ed7b867fe9faa22444",
+    "url": "/FunFrontend/static/js/runtime-main.6340fe3c.js"
+  },
+  {
+    "revision": "047a99afbf37515354fd8cef14880bc1",
+    "url": "/FunFrontend/static/media/account.047a99af.png"
+  },
+  {
+    "revision": "ea80e8bcf70c0a180cdc6905afb3f1c1",
+    "url": "/FunFrontend/static/media/background-website.ea80e8bc.jpg"
+  },
+  {
+    "revision": "b80a4a2edf38447ca3437cebdb6bf5ac",
+    "url": "/FunFrontend/static/media/building.b80a4a2e.png"
+  },
+  {
+    "revision": "433b4564e9baa47012a7d792c8e6c87b",
+    "url": "/FunFrontend/static/media/calendar.433b4564.png"
+  },
+  {
+    "revision": "aa482cab716117ab37b44a6407c49224",
+    "url": "/FunFrontend/static/media/clock.aa482cab.png"
+  },
+  {
+    "revision": "62cfb823433a1270ec8f767e95843e59",
+    "url": "/FunFrontend/static/media/plane.62cfb823.png"
+  }
+]);
